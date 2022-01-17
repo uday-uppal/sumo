be1064be2c0aacf6ac0e4434385d75b2ac19315c
uday <udayuppal2@gmail.com>
2022-01-18 00:54:24 +0530

    2

--- s.py
+++ s.py
@@ -15,16 +15,58 @@ def single(path,vehicles):
         tree.write(f)
     os.system("sumo -c grid.sumocfg --device.fcd.period 100")
 
-    if __name__ == '__main__':
-        print("Done")
-        import os
-        import numpy as np
-        import analysis
-        from matplotlib import pyplot as plt
+def textify(vehicles):
+    tree = ET.parse("grid.output"+str(vehicles)+".xml")
+    root = tree.getroot()
 
-        import xml.etree.ElementTree as ET
-        path = "C:\Hackers Realm\iit bombay techfest\sumo1\sumo"
-        initialize()
-        vehicles_arr=np.r_[np.linspace(10,100,10).astype(int),np.linspace(100,2000,20).astype(int)]
-        for i in range(0,len(vehicles_arr)):
-            single(path,vehicles_arr[i])
+    l = 0
+    for child in root:
+        for child2 in child:
+            l += 1
+
+    c = 0
+    t = 0
+    a = ''
+    speeds = np.zeros(l)
+    times = np.zeros(l)
+    ids = np.zeros(l)
+    for child in root:
+        for child2 in child:
+            # print(child2.tag, child2.attrib)
+            if (child2.tag == 'vehicle'):
+                a = (child2.attrib)
+                speeds[c] = np.float(a['speed'])
+                ids[c] = np.float(a['id'])
+                times[c] = t
+                c = c + 1
+        t = t + 1
+        # print(t)
+    data = np.c_[ids, times, speeds]
+
+    tt = len(np.unique(data[:, 1]))
+    vel = np.zeros(tt)
+    nc = np.zeros(tt)
+    flux = np.zeros(tt)
+
+    for i in range(0, tt):
+        w = np.where(data[:, 1] == i)
+        vel[i] = np.mean(data[w, 2])
+        nc[i] = len(w[0])
+        flux[i] = np.sum(data[w, 2])
+        velm = np.c_[nc, vel, flux]
+
+    np.savetxt('velm'+str(vehicles)+'.txt',velm)
+
+if __name__ == '__main__':
+    print("Done")
+    import os
+    import numpy as np
+    import analysis
+    from matplotlib import pyplot as plt
+
+    import xml.etree.ElementTree as ET
+    path = "C:\\Hackers Realm\\iit bombay techfest\\sumo1\\sumo"
+    initialize()
+    vehicles_arr=np.r_[np.linspace(10,100,10).astype(int),np.linspace(100,2000,20).astype(int)]
+    for i in range(0,len(vehicles_arr)):
+        single(path,vehicles_arr[i])
